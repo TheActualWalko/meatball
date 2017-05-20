72e89cebf39d385d1500b667e6aa3d3e35f16655
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-05-20 14:28:49 -0400

    placeholder wall-adding

--- public/src/main.js
+++ public/src/main.js
@@ -1,11 +1,13 @@
 import store from "./state/store";
 import Renderer from "./view/renderer";
 import Grid from "./view/grid";
+import Walls from "./view/walls";
 
 const initialState = store.getState();
 
 const mainRenderer = new Renderer(document.body, window.innerWidth, window.innerHeight);
 const grid = new Grid(mainRenderer.scene, initialState.getIn(["grid", "width"]), initialState.getIn(["grid", "depth"]));
+const walls = new Walls(mainRenderer.scene);
 
 mainRenderer.run();
 
@@ -14,6 +16,9 @@ window.addEventListener("resize", ()=>{
 });
 
 window.addEventListener("mousemove", evt => grid.updateCursor(evt, mainRenderer.camera));
+window.addEventListener("mousedown", evt => {
+  console.log(grid.getMousePosition(evt, mainRenderer.camera));
+});
 
 store.subscribe((...args)=>{
   const state = store.getState();
---
+++ public/src/view/walls.js
@@ -0,0 +1,8 @@
+import * as THREE from "three";
+
+class Walls {
+  constructor(scene) {
+  }
+}
+
+export default Walls;
